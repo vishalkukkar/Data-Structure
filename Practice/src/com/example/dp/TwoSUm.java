44a1ce53c17f37434fd0aab03f980d9eaa0989fa
Vishal Kukkar <vkukkar@equilar.com>
2017-09-12 21:07:02 -0700

    all two sums indexes

--- Practice/src/com/example/dp/TwoSUm.java
+++ Practice/src/com/example/dp/TwoSUm.java
@@ -1,40 +1,63 @@
 package com.example.dp;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+/*count of pairs which gives the sum to target
+like 2+5 =7
+is one pair 
+and 3+4 = 7 another
+so count is 2 for below array
+{2,5,3,4,6}
+when target is 7
+*/
 public class TwoSUm {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException {
 
-		int[] array = { 2, 11, 15,7 };
-		System.out.println(Arrays.toString(twoSumMap(array, 9)));
+		int[] array = {2,5,3,4,6};
+		ObjectMapper m = new ObjectMapper();
+		System.out.println(m.writeValueAsString(twoSumMap(array, 7)));
 	}
-	
-	
-	public static int[] twoSumMap(int[] nums, int target) {
-		
-		int[] result = new int[2];
-		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-	
+
+	public static List<List<Integer>> twoSumMap(int[] nums, int target) {
+
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+
 		for (int i = 0; i < nums.length; i++) {
 			
-			
-			if(map.containsKey(nums[i])){
-				result[0] = map.get(nums[i]);
-				result[1] = i;
-				return result;
-				
-			}else{
-				map.put(target - nums[i], i);
-			}
-			
-		}
-		return result;
-	}
-	
+			if (map.containsKey(nums[i])) {
+				List<Integer> temp = map.get(nums[i]);
+				List<Integer> temp2 = new ArrayList<Integer>();
+				temp2.add(i);
+				temp2.add(temp.get(0));
+				list.add(temp2);
+				temp.remove(0);
+				map.put(nums[i], temp);
 
-	public static int[] twoSum(int[] nums, int target) {
+			} else {
+				if (map.containsKey(target - nums[i])) {
+					List<Integer> index = map.get(target - nums[i]);
+					index.add(i);
+					map.put(target - nums[i], index);
+				} else {
+					List<Integer> index = new ArrayList<>();
+					index.add(i);
+					map.put(target - nums[i], index);
+				}
+			}
+		}
+		return list;
+	}
+	/*public static int[] twoSum(int[] nums, int target) {
 
 		int val = 0;
 		int[] result = new int[2];
@@ -53,5 +76,5 @@ public class TwoSUm {
 		return null;
 
 	}
-
+*/
 }
